0ffe178855c874a6ab2208975fb5b92f71f42c25
Afianf <alfianhc000@gmail.com>
2022-07-10 00:22:15 +0700

    Update Welcome.php

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -22,4 +22,12 @@ class Welcome extends CI_Controller {
 	{
 		$this->load->view('welcome_message');
 	}
+	public function ya()
+	{
+		echo "yayaya";
+	}
+	public function info()
+	{
+		phpinfo();
+	}
 }
